f61f5a46368bc7a1ed30e73dce34dccb54ca291f
aaradhya <aaradhyanepal1@gmail.com>
2024-12-14 23:09:16 +0545

    update: use next image in about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -53,10 +53,12 @@ const About = () => {
             </div>
             <div className="h-dvh w-screen" id="clip">
                 <div className="mask-clip-path about-image">
-                    <img
-                        src="img/about.webp"
+                    <Image
+                        src="/img/about.webp"
                         alt="Background"
                         className="absolute left-0 top-0 size-full object-cover"
+                        height={2400}
+                        width={2400}
                     />
                 </div>
             </div>
